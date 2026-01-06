9c0ab04ac6f52cd724a35ee950d0d09d879fe23f
Evelyn Mwanunura <evelynmwanunura@gmail.com>
2026-01-06 11:31:41 +0200

    Add event listeners

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+let name = document.getElementById("name");
+let accountNumber = document.getElementById("accountNumber");
+let balance = document.getElementById("balance");
+let depositBtn = document.getElementById("deposit");
+let withdrawBtn = document.getElementById("withdraw");
+let checkBalanceBtn = document.getElementById("balance");
+let message = document.getElementById("message");
+ 
 class Account{
     constructor(name, balance,  currency ) {
         this.name = name;
@@ -6,21 +14,43 @@ class Account{
     }
 
     deposit(a){
+        
         this.balance  += a
-        return  `your new  balance is: ${this.balance}`
+        message.innerText = `your new  balance is:  ${this.currency}${this.balance}`
+        
     }
 
     withdraw(b) {
         this.balance -= b
-        return `you have withdrawn ${b}, your new balance is ${this.balance}`
+        message.innerText = `you have withdrawn ${b}, your new balance is ${this.currency}${this.balance}`
     }
 
     checkBalance(){
-        return `your balance is: ${this.balance}`
+        message.innerText = `your balance is: ${this.currency}${this.balance}`
     }
 }
 
 const EvelynAccount = new Account("Evelyn Mwanunura  ", 500, "USD");
 
-EvelynAccount.checkBalance;
-EvelynAccount.deposit(300);
+function displayAccountInfo(){
+    name.innerText = `Name: ${EvelynAccount.name}`;
+    accountNumber.innerText = `Account Number: 123456789`;
+    balance.innerText = `Balance: ${EvelynAccount.currency}${EvelynAccount.balance}`;
+}
+
+displayAccountInfo();
+
+depositBtn.addEventListener("click", function() {
+    let a = parseFloat(prompt("Enter amount to deposit:"));
+    EvelynAccount.deposit(a);
+});
+
+withdrawBtn.addEventListener("click", function() {
+    
+    let b = parseFloat(prompt("Enter amount to withdraw:"));
+    EvelynAccount.withdraw(b);
+});
+
+checkBalanceBtn.addEventListener("click", function() {
+    EvelynAccount.checkBalance();
+});
